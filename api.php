92327bd049abd4d7d1f2d9086931f2a636a9f99e
Daniel Pilloni <hello@stationzero.dk>
2025-04-23 13:17:31 +0200

    php

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 //include 'db.php';
-
-header("Content-Type: application/json");
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 $jwt_token = "";
 
 function b64Encoding($data) {
