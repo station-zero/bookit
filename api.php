8aa595eaf3a54cd0475d2cbc9a44dd97fdd3d6b6
Daniel Pilloni <hello@stationzero.dk>
2025-04-24 17:09:33 +0200

    php

--- api.php
+++ api.php
@@ -2,6 +2,8 @@
 //include 'db.php';
 header('Access-Control-Allow-Origin: *');
 
+$db = new SQLite3('database.db');
+
 $jwt_token = "";
 
 function b64Encoding($data) {
@@ -35,19 +37,65 @@ $jwt_token = jwt($token, $secret, date("c"));
 $method = $_SERVER['REQUEST_METHOD'];
 $input = $_POST;
 
+function send_mail($email, $code)
+{
+    $subject = 'Hello!';
+
+    $headers['From'] = 'noreply@apoint.dk';
+    $headers['MIME-Version'] = 'MIME-Version: 1.0';
+    $headers['Content-type'] = 'text/html; charset=iso-8859-1';
+
+    $message = '
+    <html>
+    <head>
+        <title>Verify my ass...</title>
+    </head>
+    <body>
+        <p>CLick on that F***ing link!:</p>
+        <a href="https://www.apoint.dk/verify.php?i=' . $code . '">Click HERE </a>
+    </body>
+    </html>
+    ';
+
+    mail($email, $subject, $message, $headers);
+}
+
 switch ($method) {
     case 'POST':
         if($input['action']=="login")
         {
+            $username = $input['username'];
+            $password = $input['password'];
+           
+            
+            
             echo json_encode(["message" => "successfully1 ", "JWT" => $jwt_token]); 
         }
         
         if($input['action']=="new_account")
         {
             //create new account
-            echo json_encode(["message" => "successfully1 ", "JWT" => $jwt_token]); 
+            $username = $input['username'];
+            $password = $input['password'];
+            $email = $input['email'];
+            $hashed_pw = hash("sha256", $password);
+            $verified_code = base64_encode(random_bytes(12));
+
+            $results = $db->prepare('INSERT INTO users(username, email, hashed_password, verified) VALUES (?,?,?,?)');
+            $results->bindValue(1, $username);
+            $results->bindValue(2, $email);
+            $results->bindValue(3, $hashed_pw);
+            $results->bindValue(4, $verified_code);
+            $results->execute();
+             
+            if(!$results){
+                echo json_encode(["message" => $db->lastErrorMsg()]);
+            }else{
+                send_mail($email,$verified_code);
+                echo json_encode(["message" => "OKAY:" . $verified_code]);
+            }
         }
-        
+
         if($input['action']=="get_boards")
         {
             $boards = array();
@@ -74,5 +122,4 @@ switch ($method) {
         break;
 }
 
-$conn->close();
 ?>
