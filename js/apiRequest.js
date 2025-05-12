6a8ce73a46641bb438b43a6347e12f656b9e9731
Daniel Pilloni <hello@stationzero.dk>
2025-05-12 22:54:15 +0200

    change if statements to cases in render.js and apiRequest.js
    change if statements to cases in render.js and apiRequest.js

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -3,219 +3,200 @@ function apiRequest(action,parameter)
     let data_obj = {};
     let type = "";
 
-    if( action == "login")
-    {	
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'email': $("#login_email").val(),
-            'password': $("#login_password").val()
-        }
-    }
-
-    if(action == "new_account")
+    switch(action)
     {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'email': $("#account_email").val(),
-            'password': $("#account_password").val(),
-            'username': $("#account_username").val()
+        case "login":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'email': $("#login_email").val(),
+                'password': $("#login_password").val()
             }
-    }
+        break;
 
-    if(action == "delete_userprofile")
-    {
-        profile.setLoginStatus(false);
+        case "new_account":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'email': $("#account_email").val(),
+                'password': $("#account_password").val(),
+                'username': $("#account_username").val()
+                }
+        break;
+
+        case "delete_userprofile":
+            profile.setLoginStatus(false);
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token': profile.getJWT()
+            }
+        break;
+
+        case "save_dates":
+            let startInput = $("#startDate").val();
+            let endInput = $("#endDate").val();
+            let start = startInput.split("/");
+            let end = endInput.split("/");
         
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token': profile.getJWT()
-        }
-    }
+            type = "POST";        
+            data_obj = {
+                'action':action,
+                'token': profile.getJWT(),
+                'start': String(start[2]).padStart(2,"0") + "-" + String(start[1]).padStart(2,"0") + "-" + start[0] + " 00:00",
+                'end': String(end[2]).padStart(2,"0") + "-" + String(end[1]).padStart(2,"0") + "-" + end[0] + " 00:00",
+                'calendar_id':profile.getCalendarID()
+            }
+            break;
 
-    if(action == "save_dates")
-    {
-        let startInput = $("#startDate").val();
-        let endInput = $("#endDate").val();
-        let start = startInput.split("/");
-        let end = endInput.split("/");
-        
-        type = "POST";        
-        data_obj = {
-            'action':action,
-            'token': profile.getJWT(),
-            'start': String(start[2]).padStart(2,"0") + "-" + String(start[1]).padStart(2,"0") + "-" + start[0] + " 00:00",
-            'end': String(end[2]).padStart(2,"0") + "-" + String(end[1]).padStart(2,"0") + "-" + end[0] + " 00:00",
-            'calendar_id':profile.getCalendarID()
-        }
-    }
-
-    if( action == "remove_booking")
-    {
-        type = "POST";
-        data_obj = {
+        case "remove_booking":
+            type = "POST";
+            data_obj = {
                 'action': action,
                 'id': parameter,
                 'calendar_id': profile.getCalendarID,
                 'token': profile.getJWT()
-        }
-    }	
-    
-    if(action == "save_timeslots")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token': profile.getJWT(),
-            'time_slots': parameter,
-            'calendar_id':profile.getCalendarID()
-        }
-    }
-
-    if(action == "add_user")
-    {
-        type = "POST";
-        data_obj = {
-            'action': action,
-            'email': $("#add_user_email").val(),
-            'calendar_id':profile.getCalendarID(),
-            'token': profile.getJWT()
-        }
-    }
-
-    if(action == "remove_user")
-    {
-        type = "POST";
-        data_obj = {
-            'action': action,
-            'id': parameter,
-            'calendar_id':profile.getCalendarID(),
-            'token': profile.getJWT()
-        }
-    }
-
-    if(action == "remove_pending_user")
-    {
-        type = "POST";
-        data_obj = {
-            'action': action,
-            'id': parameter,
-            'calendar_id':profile.getCalendarID(),
-            'token': profile.getJWT()
-        }
-    }    
-    
-    if(action == "remove_calendar")
-    {
-        type = "POST";
-        data_obj = {
-            'action': action,
-            'calendar_id':profile.getCalendarID(),
-            'token': profile.getJWT()
-        }
-    }
-
-    if( action == "new_calendar")
-    {
-        type = "POST";
-        data_obj = {
-            'action': action,
-            'title': $("#c_title").val(),
-            'type': $("#c_type").val(),
-            'interval': parseInt($("#c_interval").val()),
-            'token': profile.getJWT()
-        }
-    }	
-
-    if( action == "view_calendars")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT()
-        }
-    }
-
-    if( action == "new_password")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT(),
-            'new_password':$("#new_password").val()
-        }
-    }
-    
-    if( action == "calendar")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT(),
-            'id':parameter
-        }
-    }
-
-    if( action == "calendar_settings")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT(),
-            'id':parameter
             }
-    }
+            break;
 
-    if( action == "reset")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'email':$("#reset_email").val()
+        case "save_timeslots":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token': profile.getJWT(),
+                'time_slots': parameter,
+                'calendar_id':profile.getCalendarID()
             }
-    }
-    
+            break;
 
-    if( action == "check_email")
-    {    
-        type = "GET";
-        data_obj = {
-            'action':action,
-            'email':parameter
+        case "add_user":
+            type = "POST";
+            data_obj = {
+                'action': action,
+                'email': $("#add_user_email").val(),
+                'calendar_id':profile.getCalendarID(),
+                'token': profile.getJWT()
             }
-    }
+            break;
+
+        case "remove_user":
+            type = "POST";
+            data_obj = {
+                'action': action,
+                'id': parameter,
+                'calendar_id':profile.getCalendarID(),
+                'token': profile.getJWT()
+            }
+            break;
+
+        case "remove_pending_user":
+            type = "POST";
+            data_obj = {
+                'action': action,
+                'id': parameter,
+                'calendar_id':profile.getCalendarID(),
+                'token': profile.getJWT()
+            }
+            break;
+        
+        case "remove_calendar":
+            type = "POST";
+            data_obj = {
+                'action': action,
+                'calendar_id':profile.getCalendarID(),
+                'token': profile.getJWT()
+            }
+            break;
+
+        case "new_calendar":
+            type = "POST";
+            data_obj = {
+                'action': action,
+                'title': $("#c_title").val(),
+                'type': $("#c_type").val(),
+                'interval': parseInt($("#c_interval").val()),
+                'token': profile.getJWT()
+            }
+            break;
+        
+        case "view_calendars":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT()
+            }
+            break;
+            
+        case "new_password":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT(),
+                'new_password':$("#new_password").val()
+            }
+            break;
+            
+        case "calendar":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT(),
+                'id':parameter
+            }
+            break;
+            
+        case "calendar_settings":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT(),
+                'id':parameter
+            }
+            break;
+            
+        case "reset":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'email':$("#reset_email").val()
+            }
+            break;
     
-    if( action == "get_messages")
-    {
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT()
-        }
+        case "check_email":
+            type = "GET";
+            data_obj = {
+                'action':action,
+                'email':parameter
+            }
+            break;
+        
+        case "get_messages":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT()
+            }
+            break;
+        
+        case "send_message":
+            const msg = $("#msg").val();
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT(),
+                'message':msg,
+                'receiver':profile.getReceiver()
+            }
+            break;
+    
+        case "logout":
+            type = "POST";
+            data_obj = {
+                'action':action,
+                'token':profile.getJWT()
+            }
+            break;
     }
 
-    if( action == "send_message")
-    {
-        const msg = $("#msg").val();
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT(),
-            'message':msg,
-            'receiver':profile.getReceiver()
-        }
-    }
-    
-    if(action == "logout")
-    {    
-        type = "POST";
-        data_obj = {
-            'action':action,
-            'token':profile.getJWT()
-         }
-    }
-    
     callAPI(action, data_obj, type);
 }
--- js/render.js
+++ js/render.js
@@ -10,13 +10,7 @@ function renderPage(route, val)
     $("#new_password").val("");
     $(".toogle_tab").hide();
 
-    if(route=="error")
-    {
-        page="#error";
-        $("#error_msg").html(val);
-    }
-        
-    if(route=="loggedin")
+    if(route == "loggedin")
     {
         profile.setJWT(val['token']);
         profile.setID(val['id']);
@@ -32,102 +26,96 @@ function renderPage(route, val)
         }
     }
 
-    if(route=="new_message")
-    {
-        setReceiver(val);
-        page = "#messages";
-    }
-
-    if(route=="message_page")
-    {
-        renderMessages(val);
-        page = "#messages";        
-    }
+    switch (route) {
+        case "error":
+            page="#error";
+            $("#error_msg").html(val);
+            break;
     
-    if(route=="#calendar")
-    {
-        apiRequest("calendar",val);
-        profile.setCalendarID(val);
-        page = "#load";
-    }
-
-    if(route=="calendars_list")
-    {
-        calendarList(val);
-        page = "#calendars";
-    }
-
-    if(route=="#settings")
-    {
-        apiRequest("calendar_settings",val);
-        profile.setCalendarID(val);
-        page = "#load";
-    }
-
-    if(route=="settings_view")
-    {
-        settings(val);
-        page = "#settings_view";
-    }
-
-    if(route=="#success")
-    {
-        $("#success_msg").html(val);
-        page = "#success";
-    }
+        case "new_message":
+            profile.setReceiver(val);
+            page = "#messages";
+            break;
     
-    if(route=="goto")
-    {
-        if(val == ""){
-            if(app==false){
-                val = "#home";
-            }else{
-                val = "#calendars";
-            }
-        }
+        case "message_page":
+            renderMessages(val);
+            page = "#messages";
+            break;
+    
+        case "#calendar":  
+            apiRequest("calendar",val);
+            profile.setCalendarID(val);
+            page = "#load";
+            break;
 
-        if(val == "#calendars")
-        {
-            apiRequest("view_calendars","");
-            val="#load";
-        }
+        case "calendars_list":
+            calendarList(val);
+            page = "#calendars";
+            break;
 
-        if(val=="dm")
-        {
-            apiRequest("get_messages","");
-            val = "#load";
-        }
+        case "#settings":
+            apiRequest("calendar_settings",val);
+            profile.setCalendarID(val);
+            page = "#load";
+            break;
 
-        if(val=="#messages")
-        {
-            profile.setReceiver(null);
-            apiRequest("get_messages","");
-            val = "#load";
-        }
+        case "settings_view":
+            settings(val);
+            page = "#settings_view";
+            break;
+        
+        case "#success":
+            $("#success_msg").html(val);
+            page = "#success";
+            break;
 
-        if(val=="#logout")
-        {
-            profile.setLoginStatus(false);
-            apiRequest("logout","");
-            val = "#load";
-        }
+        case "calendar_view":
+            calendar(val['bookings'],val['title']);
+            page="#calendar";
+            break;
 
+        case "time_picker_view":
+            timePicker(val['interval'], val['bookings'], getScreen());
+            page="#time_picker";
+            break;
+            
+        case "goto":
+            switch (val) {
+            case "":
+                if(app==false){
+                    val = "#home";
+                }else{
+                    val = "#calendars";
+                }
+                break;
+
+            case "#calendars":
+                apiRequest("view_calendars","");
+                val="#load";
+                break;
+
+            case "dm":
+                apiRequest("get_messages","");
+                val = "#load";
+                break;
+
+            case "#messages":
+                profile.setReceiver(null);
+                apiRequest("get_messages","");
+                val = "#load";
+                break;
+
+            case "#logout":
+                profile.setLoginStatus(false);
+                apiRequest("logout","");
+                val = "#load";
+                break;
+            
+           
+        }
         page = val;
     }
-
-    if(route=="calendar_view")
-    {
-        calendar(val['bookings'],val['title']);
-        page="#calendar";
-    }
-
-    if(route=="time_picker_view")
-    {
-        timePicker(val['interval'], val['bookings'], getScreen());
-        page="#time_picker";
-
-    }
-
+    
     if(page!="")
     {
         if(page=="#calendar" || page=="#calendars" || page=="#messages" || page=="#time_picker" || page=="#profile" || page=="#create_calendar" || page=="#settings_view")
